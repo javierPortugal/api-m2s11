e55afa88fa7c32b04c65dd0eb00aac76a74355a0
Javier Portugal <javier.s.portugal@gmail.com>
2023-11-20 22:07:49 -0600

    update js
    inclusion de respuestas

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,18 @@
 console.log("Entro index.js");
+
+//antes se usaban liberarias como axios
+//ahora javascript tiene la libreria "fetch"
+
+fetch('https://dev4humans.com.mx/api/clases/productos', {method: "GET"})
+    .then(response =>response.json())
+ //   .then(data => console.log(data));
+     .then(data => {
+        console.log("La promesa se resolvioo");
+        const rawData = data;
+        console.log(rawData);
+        console.log(rawData.data.products[0]);
+
+
+      }).catch(error => console.log(error))
+
+
