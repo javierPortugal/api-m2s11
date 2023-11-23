0564b91116387e652c0ae10deb4a307fc9fbb049
Javier Portugal <javier.s.portugal@gmail.com>
2023-11-22 21:36:02 -0600

    update general
    se termina el ejecicio

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,47 @@
 console.log("Entro index.js");
 
+let contenidoProductos = document.getElementById("contenidoProductos");
+
+
+
+
 //antes se usaban liberarias como axios
 //ahora javascript tiene la libreria "fetch"
 
 fetch('https://dev4humans.com.mx/api/clases/productos', {method: "GET"})
+//fetch('https://dev4humans.com.mx/api/clases/productos?limit=23', {method: "GET"})
     .then(response =>response.json())
  //   .then(data => console.log(data));
      .then(data => {
         console.log("La promesa se resolvioo");
         const rawData = data;
-        console.log(rawData);
-        console.log(rawData.data.products[0]);
+        const productos = rawData.data.products;
+        //console.log(rawData);
+        console.log(productos);
+        productos.forEach(producto => {
+          //contenidoProductos.innerHTML = contenidoProductos.innerHTML + `<p>${producto.title}</p>`;
+          contenidoProductos.innerHTML = contenidoProductos.innerHTML + `
+          <div class="col-md-6">
+                <div class="card mb-3">
+                    <div class="row g-0">
+                      <div class="col-md-4">
+                        <img src="${producto.thumbnail}" class="img-fluid rounded-start" alt="...">
+                      </div>
+                      <div class="col-md-8">
+                        <div class="card-body">
+                          <h5 class="card-title">${producto.title}  - $${producto.price}</h5>
+                          <p class="card-text">${producto.description}</p>
+                          <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+            </div>
+
+          
+          `;
+
+        });
 
 
       }).catch(error => console.log(error))
